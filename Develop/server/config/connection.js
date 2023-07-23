46834a966f5c37992fdb502585a9d26962fd708f
Chester Springwood <jackieAmartinez@gmail.com>
2023-07-23 18:09:08 -0400

    mutations and queries

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-module.exports = mongoose.connection;
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("Successfully connected to the database");
+});
+
+module.exports = db;
